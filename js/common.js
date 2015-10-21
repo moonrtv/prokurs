3a330f10499ecbc2c31fee8f8a94eb84ff9ea4fe
Rodionov Taras <moonrtv@ya.ru>
2015-10-21 14:49:34 +0300

    Сompletion of typesetting.

--- js/common.js
+++ js/common.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
 
+$(".popup").magnificPopup({type:"image"});
+$(".popup_c").magnificPopup();
+
+$.stellar({
+    responsive: true,
+    horizontalOffset: 60
+  });
+
+  $(".carousel").owlCarousel({
+    responsive : {
+      0 : {
+        items : 1,
+        nav : true
+      }
+    },
+    navText : ""
+  });
+
   function wResize() {
-     $("header").css("min-height", $(window).height());
+    $("header").css("min-height", $(window).height());
   };
   wResize();
   $(window).resize(function() {
-    wResize();
+    wResize()
   });
 
   $(".top_phone .wrapper .tab").click(function() {
@@ -13,9 +31,19 @@ $(document).ready(function() {
     $(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
   }).eq(0).addClass("active");
 
+  $(".bottom_phone .wrapper .tab").click(function() {
+    $(".bottom_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+
   $(".tabs_header .wrapper .tab").click(function() {
     $(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
     $(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+
+  $(".contacts_top .tab").click(function() {
+    $(".contacts_top .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".s_contacts .tab_item").hide().eq($(this).index()).fadeIn()
   }).eq(0).addClass("active");
 
 	//Цели для Яндекс.Метрики и Google Analytics
@@ -34,18 +62,35 @@ $(document).ready(function() {
 
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#form").submit(function(e) {
-		e.preventDefault;
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-	});
+	$("form").submit(function(e) {
+    var ths = $(this);
+    e.preventDefault;
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      alert("Спасибо за заявку!");
+      setTimeout(function() {
+        var magnificPopup = $.magnificPopup.instance;
+        magnificPopup.close();
+        ths.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 
+});
+
+$(window).load(function() {
+
+  $(".loader_inner").fadeOut();
+  $(".loader").delay(400).fadeOut("slow");
+
+  $(".top_header").animated("fadeInDown", "fadeOut");
+  $(".tabs_header .wrapper").animated("flipInY", "fadeOut");
+  $(".profi_item").animated("fadeInRight", "fadeOut");
+  $(".s_profi form").animated("zoomInRight", "fadeOut");
+  $(".s_back h3").animated("fadeInUp", "fadeOut");
+  $("section h2, footer h2, .contacts_top .tabs").animated("fadeInUp", "fadeOut");
 });
